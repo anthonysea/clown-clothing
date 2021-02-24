3847e60e2930abd1ed64cacda091f822dad11180
anthonysea <anthonyqchao@gmail.com>
2021-02-24 12:09:02 -0800

    sign in and sign up page; sign in component

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+
+import './sign-in.styles.scss';
+
+const SignIn = () => {
+    const [state, setState] = useState({'email': '', 'password': ''})
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        setState({'email': '', 'password': ''})
+    }
+
+    const handleChange = event => {
+        const { value, name } = event.target;
+        setState({ ...state, [name]: value})
+    }
+
+    return (
+        <div className='sign-in'>
+            <h2>I already have an account</h2>
+            <span>Sign in with your email and password</span>
+
+            <form onSubmit={handleSubmit}>
+                
+                <FormInput name="email" label='email' value={state.email} handleChange={handleChange} required></FormInput>
+                <FormInput name="password" label='password' type="password" value={state.password} handleChange={handleChange} required></FormInput>
+
+                <CustomButton type="submit">Sign In</CustomButton>
+            </form>
+
+        </div>
+    )
+}
+
+export default SignIn;
