2e4ec34907f1c842e0c0830f973958b7ce2df0e7
anthonysea <anthonyqchao@gmail.com>
2021-04-30 15:16:42 -0700

    moved shop data to firebase; using HoC for loading spinners;

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -40,6 +40,39 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
     return userRef
 }
 
+
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+    const collectionRef = firestore.collection(collectionKey);
+    console.log(collectionRef);
+
+    // Batch write
+    const batch = firestore.batch();
+    objectsToAdd.forEach(obj => {
+        const newDocRef = collectionRef.doc(); // Get a new documentReference and randomly generate an ID
+        batch.set(newDocRef, obj);
+    });
+
+    return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = (collections) => {
+    const transformedCollections = collections.docs.map(doc => {
+        const { title, items } = doc.data();
+
+        return {
+            routeName: encodeURI(title.toLowerCase()),
+            id: doc.id,
+            title,
+            items,
+        }
+    });
+
+    return transformedCollections.reduce((accumulator, collection) => {
+        accumulator[collection.title.toLowerCase()] = collection;
+        return accumulator;
+    }, {})
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
