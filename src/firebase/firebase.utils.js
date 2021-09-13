d8e599cfa69697396ad8ffcf9789b836e51bd38c
anthonysea <anthonyqchao@gmail.com>
2021-09-13 11:56:59 -0700

    Rewriting README. Encapsulating components.

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,67 +12,6 @@ const config = {
     measurementId: "G-S9WHRQT40J"
 };
 
-
-export const createUserProfileDocument = async (userAuth, additionalData) => {
-    if (!userAuth) return;
-
-    const userRef = firestore.doc(`users/${userAuth.uid}`);
-    const snapshot = await userRef.get();
-
-    // If user doesn't exist, create it
-    if (!snapshot.exists) {
-        const { displayName, email } = userAuth;
-        const createdAt = new Date();
-
-        // Create new user
-        try {
-            await userRef.set({
-                displayName, 
-                email,
-                createdAt,
-                ...additionalData
-            })
-        } catch(error) {
-            console.log('Error creating user', error.message)
-        }
-    }
-
-    return userRef
-}
-
-
-export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
-    const collectionRef = firestore.collection(collectionKey);
-    console.log(collectionRef);
-
-    // Batch write
-    const batch = firestore.batch();
-    objectsToAdd.forEach(obj => {
-        const newDocRef = collectionRef.doc(); // Get a new documentReference and randomly generate an ID
-        batch.set(newDocRef, obj);
-    });
-
-    return await batch.commit();
-};
-
-export const convertCollectionsSnapshotToMap = (collections) => {
-    const transformedCollections = collections.docs.map(doc => {
-        const { title, items } = doc.data();
-
-        return {
-            routeName: encodeURI(title.toLowerCase()),
-            id: doc.id,
-            title,
-            items,
-        }
-    });
-
-    return transformedCollections.reduce((accumulator, collection) => {
-        accumulator[collection.title.toLowerCase()] = collection;
-        return accumulator;
-    }, {})
-}
-
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,41 +2,35 @@ import { React, useState, useEffect } from 'react'
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
-import CollectionPage from '../collection/collection.component';
-import WithSpinner from '../../components/with-spinner/with-spinner.component';
+import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions';
 
-import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
+import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
+import CollectionPageContainer from '../collection/collection.container';
 
-import { updateCollections } from '../../redux/shop/shop.actions'
 
-const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
-const CollectionsPageWithSpinner = WithSpinner(CollectionsOverview);
-
-const ShopPage = ({ match, updateCollections }) => {
-    const unsubscribeFromSnapshot = null;
-    const [isLoading, setIsLoading] = useState(true);
+const ShopPage = ({ match, fetchCollectionsStartAsync }) => {
 
     useEffect(() => {
-        const collectionRef = firestore.collection('collections');
-
-        collectionRef.onSnapshot(async snapshot => {
-            const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-            updateCollections(collectionsMap);
-            setIsLoading(false);
-        });
-    },[])
+        fetchCollectionsStartAsync();
+    }, [])
 
     return (
-        <div className='shop-page'>
-            <Route exact path={`${match.path}`} render={(props) => <CollectionsOverviewWithSpinner isLoading={isLoading} {...props} />} />
-            <Route path={`${match.path}/:collectionId`} render={(props) => <CollectionsPageWithSpinner isLoading={isLoading} {...props} />} />
-        </div>
-    )
+      <div className="shop-page">
+        <Route
+          exact
+          path={`${match.path}`}
+          component={CollectionsOverviewContainer}
+        />
+        <Route
+          path={`${match.path}/:collectionId`}
+          component={CollectionPageContainer}
+        />
+      </div>
+    );
 }
 
 const mapDispatchToProps = dispatch => ({
-    updateCollections: collectionsMap => dispatch(updateCollections(collectionsMap))
+    fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync()),
 })
 
 export default connect(null, mapDispatchToProps)(ShopPage);
